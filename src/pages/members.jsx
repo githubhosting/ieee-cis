54833b9451159e9286fd7acdca04c3c8744d814f
myselfshravan <shravanrevanna@gmail.com>
2022-11-12 16:49:11 +0530

    made shadow vissible

--- src/pages/members.jsx
+++ src/pages/members.jsx
@@ -65,7 +65,7 @@ export default function Login() {
         </title>
       </Head>
       <Header />
-      <section class="bg-white">
+      <section>
         <div class="container mx-auto px-6 py-10">
           <h1 class="text-center text-3xl font-semibold capitalize text-gray-800">
             Our <span class="text-sky-600">Executive Team</span>
